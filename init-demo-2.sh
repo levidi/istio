09427d842f114de79f99fbcfc4ab272d119e044a
Levi Menezes <leviditomazzo@gmail.com>
2020-04-07 16:01:07 -0300

    correção script sh

--- init-demo-2.sh
+++ init-demo-2.sh
@@ -2,5 +2,5 @@ kubectl apply -f 4-api.yaml \
 && kubectl apply -f 5-api-graphql.yaml \
 && kubectl apply -f 6-front-end.yaml \
 && kubectl apply -f ./istio/Gateway/1-gateway.yaml \
-&& kubectl apply -f ./istio/Gateway/2-virtual-service-graphql-and-web-app
+&& kubectl apply -f ./istio/Gateway/2-virtual-service-graphql-and-web-app.yaml
 
